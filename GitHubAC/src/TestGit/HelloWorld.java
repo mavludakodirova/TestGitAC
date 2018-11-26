5b7ceec26467bd7f7a0c72bde77bea20df21a134
Mavluda Kodirova <42846685+mavludakodirova@users.noreply.github.com>
2018-11-26 16:46:57 -0600

    Update HelloWorld.java

--- GitHubAC/src/TestGit/HelloWorld.java
+++ GitHubAC/src/TestGit/HelloWorld.java
@@ -3,6 +3,7 @@ package TestGit;
 public class HelloWorld {
 
 	public static void main(String[] args) {
+		String s = "mama";
 		System.out.println(" HI GitHub !!! ");
 		System.out.println("I got it!!!");
 	}
